d21730f7754afdc5cfecdcd23f77e278ca4dbf2a
amro22397 <amroalmutasim22@gmail.com>
2025-10-14 11:48:42 +0400

    many changes 14-october-2025 1110 before reinstall tabs

--- app/[locale]/resume/page.jsx
+++ app/[locale]/resume/page.jsx
@@ -64,7 +64,7 @@ const page = () => {
         }`}
       >
         <Tabs
-          defaultValue={localStorage.getItem("tab") || "education"}
+          defaultValue={localStorage.getItem("tab") || "real-projects-experience"}
           className={`flex flex-col xl:flex-row xl:gap-[60px] gap-[90px] xl:mt-20 mt-24
             ${locale === 'ar' && 'xl:mt-20 mt-24'}`}
         >  
